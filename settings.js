086b10099b69a016679a1759e312a0de23bf86b9
Antonio Robles <antjrobles@gmail.com>
2023-07-15 12:56:18 +0200

    custom themes

---
+++ settings.js
@@ -0,0 +1,29 @@
+// selectors
+const toggles = document.querySelectorAll(".settings [type='checkbox']");
+const radios = document.querySelectorAll(".settings [type='radio']");
+const doc = document.documentElement;
+
+// functions
+function updateSiteUi({ name, value }) {
+  if (name === "customColor") {
+    return doc.style.setProperty("--customColor", `var(--${value})`);
+  }
+  return (doc.dataset[name] = value);
+}
+
+// event listeners
+toggles.forEach((toggle) => {
+  toggle.addEventListener("change", (e) => {
+    const { name, checked } = e.target;
+    updateSiteUi({ name, value: checked });
+    localStorage.setItem(name, checked);
+  });
+});
+
+radios.forEach((radio) => {
+  radio.addEventListener("change", (e) => {
+    const { name, id } = e.target;
+    updateSiteUi({ name, value: id });
+    localStorage.setItem(name, id);
+  });
+});
